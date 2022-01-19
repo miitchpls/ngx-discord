71fe1b3b47966f8c6be25ed03e94f7cd4c27f718
Michele Greco <mitchgrek@gmail.com>
2022-01-19 11:27:09 +0100

    Sidebar css cleaning and divider semplification

--- src/app/shared/components/sidebar/sidebar.module.ts
+++ src/app/shared/components/sidebar/sidebar.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IconsModule } from '../icons/icons.module';
-import { SidebarDividerModule } from './sidebar-divider/sidebar-divider.module';
 import { SidebarIconModule } from './sidebar-icon/sidebar-icon.module';
 import { SidebarComponent } from './sidebar.component';
 
 @NgModule({
   declarations: [SidebarComponent],
-  imports: [CommonModule, SidebarDividerModule, SidebarIconModule, IconsModule],
+  imports: [CommonModule, SidebarIconModule, IconsModule],
   exports: [SidebarComponent],
 })
 export class SidebarModule {}

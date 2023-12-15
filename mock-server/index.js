29dd7736b74066bd43cd3682da0d59a5c8725257
Michele Greco <mitchgrek@gmail.com>
2023-12-15 19:29:30 +0100

    Added demo pic locally

--- mock-server/index.js
+++ mock-server/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./router");
+const path = require("path");
 
 const app = express();
 const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use("/assets", express.static(path.join(__dirname, "public")));
 app.use("/api", router);
 
 app.listen(port, () =>
